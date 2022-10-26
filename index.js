645b5bbaa0afb0174256d262ddb87fb8c19065cb
Dylan Curtis <Dcurtisj@gmail.com>
2022-10-26 14:49:02 -0700

    commit tree 0113a39c1d04138a78904a0a50bab5b24b533b74

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(string) {
+    cats.push (string)
+    return
+}
+
+function destructivelyPrependCat(string) {
+    cats.unshift (string)
+       return
+}
+
+function destructivelyRemoveLastCat(string) {
+    cats.pop (string)
+       return
+}
+
+function destructivelyRemoveFirstCat(string) {
+    cats.shift (string)
+       return
+}
+
+function appendCat(string){
+    let copyOfCats = [...cats, string ]   
+        return copyOfCats
+}
+
+function prependCat(string){
+    let copyOfCats = [string, ...cats ]
+        return copyOfCats
+}
+
+function removeLastCat(string){
+    let copyOfCats = [...cats]   
+    copyOfCats.pop()
+        return copyOfCats
+}
+
+function removeFirstCat(string){
+    let copyOfCats = [...cats]   
+    copyOfCats.shift()
+        return copyOfCats
+}
